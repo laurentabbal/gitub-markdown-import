791eb2e300bd10ea435e45628e255beb3a06d788
Laurent Abbal <laurent@abbal.com>
2021-08-16 22:20:32 +0900

    Update file.blade.php

--- file.blade.php
+++ file.blade.php
@@ -5,13 +5,13 @@ $github_document = github_import('path/contents/file.md');
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    @include('inc-meta')
-    <title>title</title>
+	@include('inc-meta')
+	<title>title</title>
 </head>
 <body data-spy="scroll" data-target="#navbar-scrollspy" data-offset="150">
 	@include('inc-nav')
 	<div class="container mt-5 mb-5">
-	
+
 		<div class="row">
 			<div class="col-md-3">
 				<?php echo $github_document['menu'] ?>
@@ -20,7 +20,7 @@ $github_document = github_import('path/contents/file.md');
 				<?php echo $github_document['content'] ?>
 			</div>
 		</div>
-		
+
 	</div><!-- /container -->
 	@include('inc-bottom-js')
 </body>
